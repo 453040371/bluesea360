2fd00bf12a35fb36222cd727f3c5a9a8594ccbfc
haowu <453040371@.qq.com>
2017-12-29 16:17:38 +0800

    增加联系方式,去掉敏感字,去掉广告

--- Ev_JinSe/Controllers/ArticleController.cs
+++ Ev_JinSe/Controllers/ArticleController.cs
@@ -23,90 +23,18 @@ namespace Ev_JinSe.Controllers
             return View();
         }
 
-        //
-        // GET: /Article/Details/5
+        public ActionResult Detail(string hs) {
+            if (hs == "1s")
+            {
+                ViewBag.ssss = "<h1>sdjhkjsdhkjadfhgdfkjh</h1>";
+            }
+            else {
+                string str = "<div style=\" skjfsdfjh  \">";
 
-        public ActionResult Details(int id)
-        {
+                ViewBag.ssss = "1231231111111111111111111";
+            }
+            
             return View();
-        }
-
-        //
-        // GET: /Article/Create
-
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        //
-        // POST: /Article/Create
-
-        [HttpPost]
-        public ActionResult Create(FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add insert logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        //
-        // GET: /Article/Edit/5
-
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        //
-        // POST: /Article/Edit/5
-
-        [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add update logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        //
-        // GET: /Article/Delete/5
-
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        //
-        // POST: /Article/Delete/5
-
-        [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add delete logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
         }
     }
 }
